542cafcc4c0b6ee8313add3f02c6b2164dac1ec2
Brandon Domingue <getsaf@gmail.com>
2020-02-15 17:47:05 -0500

    v9.0.1-2

--- update.sh
+++ update.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -e
 rm -rf shallow-render
-git clone https://github.com/getsaf/shallow-render.git
+BRANCH=${1:-master}
+echo "Using branch: ${BRANCH}"
+git clone --single-branch --branch ${BRANCH} https://github.com/getsaf/shallow-render.git
 
 rm -rf examples
-cp -r ./shallow-render/projects/shallow-render/src/lib/examples .
+cp -r ./shallow-render/lib/examples .
 for spec in ./examples/*.spec.ts
 do
   echo "import './$(basename ${spec%.*})';" >> examples/index.ts
 done
 
 sed -i '' "s/import {.*Shallow.*} from .*;/import { Shallow } from 'shallow-render';/" ./examples/*.spec.ts
-npm i -D shallow-render@latest
+npm i shallow-render@next
